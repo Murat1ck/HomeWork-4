378c04faca179222162892113e0a80dc4c39d06a
Murat1ck <muratoff101088@gmail.com>
2022-09-29 19:36:34 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,30 @@ public class Main {
 
         //int age = 17;
         //if (age >= 18) {
-           // System.out.println("Ты можешь водить");
+        // System.out.println("Ты можешь водить");
         //}
         //if (age < 18) {
-            //System.out.println("Ты не можешь водить");
+        //System.out.println("Ты не можешь водить");
         //}
-        System.out.println("Задание №1");
-        int age = 17;
-        if (age >= 18) {
-            System.out.println("Поздравляю с совершеннолетием!");
-        }
-        if (age < 18) {
-            System.out.println("Возраст совершеннолетия ещё не наступил :(");
-        }
+        //System.out.println("Задание №1");
+        //int age = 17;
+        //if (age >= 18) {
+        //System.out.println("Поздравляю с совершеннолетием!");
+        //}
+        //if (age < 18) {
+        //System.out.println("Возраст совершеннолетия ещё не наступил :(");
 
+        System.out.println("Задание №2");
+
+        int age = 16;
+        if (age >= 7 ) {
+            System.out.println("Ребёнок ходит в школу");
+        }
+        if (age >= 18 ) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        if (age >= 24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
     }
 }
