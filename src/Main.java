5fcc58711a4ab6bb2945bde2e74f3d72a670ae34
Murat1ck <muratoff101088@gmail.com>
2022-09-29 20:09:10 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -18,17 +18,39 @@ public class Main {
         //if (age < 18) {
         //System.out.println("Возраст совершеннолетия ещё не наступил :(");
 
-        System.out.println("Задание №2");
+        //System.out.println("Задание №2");
 
-        int age = 16;
-        if (age >= 7 ) {
-            System.out.println("Ребёнок ходит в школу");
+       // int age = 16;
+        //if (age >= 7 ) {
+           // System.out.println("Ребёнок ходит в школу");
+        //}
+       // if (age >= 18 ) {
+            //System.out.println("Человек уже закончил школу и может отправляться в университет");
+       // }
+       // if (age >= 24) {
+            //System.out.println("Человек окончил университет и ему пора искать первую работу");
+        int capacity = 102;
+        int totalSeats = 60;
+        int totalStandingPlaces = capacity - totalSeats;
+        System.out.println("Стоячих мест: " + totalStandingPlaces);
+        int sittingPlace = 61;
+        int standingPlace =43;
+        if (sittingPlace <= 60 || standingPlace <= 42) {
+            System.out.println("В вагоне есть свободные места");
         }
-        if (age >= 18 ) {
-            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        if (sittingPlace > 60 || standingPlace > 42) {
+            System.out.println("В вагоне нет свободных мест");
+
         }
-        if (age >= 24) {
-            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        if (sittingPlace > 60 && standingPlace <= 42) {
+            System.out.println("В вагоне есть стоячие места");
         }
-    }
-}
+        if (sittingPlace <= 60 && standingPlace > 42) {
+            System.out.println("В вагоне есть сидячие места");
+        }
+
+
+
+       }
+   }
+
